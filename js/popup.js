04d25586d446eadba9060dba9645241c98ccb04a
Muhammad Fadhil <33900154+devfdL@users.noreply.github.com>
2019-12-25 17:25:47 +0700

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function contactOpen() {
+    document.getElementById("contact").style.display = "block";
+}
+  
+function closeCntc() {
+    document.getElementById("contact").style.display = "none";
+}
